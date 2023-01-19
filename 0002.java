73eb0135aecf8621cab4af60d737f03c6f8bdbcc
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-01-19 19:19:55 +0400

    Update 0002.java

--- 0002.java
+++ 0002.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 public class Level1 {
         public static int odometer(int[] oksana) {
-        int speed = oksana[0];
-        int time = oksana[1];
-        for (int index = 2; index < oksana.length; index++) {
-            if (index % 2 != 0) {
-                time += oksana[index];
-            } else speed += oksana[index];
+        int distance = 0;
+        int time = 0;
+        for (int index = 0; index < oksana.length; index +=2) {
+            distance += oksana[index] * (oksana[index+1] - time);
+            time = oksana[index+1];
         }
-        return speed * time;
+        return distance;
     }
 }
