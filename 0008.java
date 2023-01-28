13a905d6da023bfc91e00b5fe10fcf97c211b147
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-01-28 20:05:22 +0400

    Update 0008.java

--- 0008.java
+++ 0008.java
@@ -3,7 +3,10 @@ import java.util.*;
 public  classLevel1 {    
     public static int SumOfThe(int N, int[] data) {
         int sum = 0;
-        int allSumOfArray = Arrays.stream(data).sum();
+        int allSumOfArray = 0;
+        for (int indexForSum : data) {
+            allSumOfArray+=indexForSum;
+        }
         for (int index : data) {
             if (index * 2 == allSumOfArray) {
                 sum = index;
