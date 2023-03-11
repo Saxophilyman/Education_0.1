c2a1e0295464e5b3781fc6b0b1322f5f9d67c871
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-03-11 15:08:40 +0400

    Update 0015.java

--- 0015.java
+++ 0015.java
@@ -2,24 +2,30 @@ import java.util.*;
 
 public class Level1 {
     public static boolean TankRush(int h1, int w1, String s1, int h2, int w2, String s2) {
-        String[] arr1 = s1.split(" ");
-        String[] arr2 = s2.split(" ");
         if (h1 < h2 || w1 < w2) {
             return false;
         }
-        boolean result = false;
+        String[] arr1 = s1.split(" ");
+        String[] arr2 = s2.split(" ");
         for (int i = 0; i <= arr1.length - arr2.length; i++) {
-            if (arr1[i].contains(arr2[0])) {
-                int firstCord = arr1[i].indexOf(arr2[0]);
-                for (int k = i, j = 0; k < i + h2; k++, j++) {
-                    if (arr1[k].indexOf(arr2[j]) != firstCord) {
-                        result = false;
-                        break;
+            int nextCord = 0;
+            int checkCord = arr1[i].indexOf(arr2[0], nextCord);
+            int checkLine = 1;
+            int checkIn = 1;
+            while (checkCord != -1) {
+                if (arr1[i + checkLine].indexOf(arr2[checkLine], checkCord) != checkCord) {
+                    checkLine = 1;
+                    nextCord = checkCord + 1;
+                    checkCord = arr1[i].indexOf(arr2[0], nextCord);
+                } else {
+                    checkLine++;
+                    checkIn++;
+                    if (checkIn == h2) {
+                        return true;
                     }
-                    result = true;
                 }
             }
         }
-        return result;
+        return false;
     }
 }
