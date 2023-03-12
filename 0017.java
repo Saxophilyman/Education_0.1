73713dd7b15aa0a0a82429aec9bb1c149bc772b3
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-03-12 17:32:59 +0400

    Update 0017.java

--- 0017.java
+++ 0017.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Level1 {
     public static boolean LineAnalysis(String line) {
         String arr1[] = line.split("\\*");
-        for (int index = 2; index < arr1.length - 1; index++) {
+        for (int index = 2; index < arr1.length; index++) {
             if (!arr1[1].equals(arr1[index])) {
                 return false;
             }
