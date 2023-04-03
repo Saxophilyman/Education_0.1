820d3f48c93f8bc3a4af00658047488d2d1cdccb
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2023-04-03 21:39:23 +0400

    Update 0025.java

--- 0025.java
+++ 0025.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class Level1 {
-    public static boolean TransformTransform(int A[], int N) {
+       public static boolean TransformTransform(int A[], int N) {
         int sum = 0;
         int[] resArr = transform(transform(A));
-        for (int x : resArr) {
-            sum += resArr[x];
+        for (int j : resArr) {
+            sum += j;
         }
         return sum % 2 == 0;
     }
@@ -13,16 +13,16 @@ public class Level1 {
     private static int[] transform(int[] arr) {
         ArrayList<Integer> array = new ArrayList<>();
         int k;
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                int tr = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr.length - 1 - i; j++) {
+                int tr = arr[j];
                 k = i + j;
-                for (int m = j; m < k; m++) {
+                for (int m = j; m <= k; m++) {
                     if (tr < arr[m]) {
                         tr = arr[m];
                     }
                 }
-                array.add(tr);
+                array.add(0,tr);
             }
         }
         int[] returnArray = new int[array.size()];
